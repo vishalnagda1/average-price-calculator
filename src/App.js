609109fe8b983759c2a8ac865b0412aad1dd69f6
Vishal Nagda <vishalnagda1@gmail.com>
2020-02-06 11:55:31 +0530

    create basic structure for BasicUI component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BasicUI from './Calculator';
 
 class App extends Component {
   render() {
@@ -11,8 +12,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To calculate the share's average price.
         </p>
+        <BasicUI />
       </div>
     );
   }
